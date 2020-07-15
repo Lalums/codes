ff7fd5a229df336e7d54a011383ef2ba2589f9b2
ratheeshkumarms <ratheesh.kumarms@xe04.ey.com>
2020-07-15 19:55:01 +0530

    change

--- remote-backend.tf
+++ remote-backend.tf
@@ -2,9 +2,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "storage"
-    storage_account_name = "jpastorageacc9898"
+    storage_account_name = "csg10032000cd959f89"
     container_name       = "statefile"
     key                  = "statefile.terraform.tfstate"
-    access_key           = "bYc3OJKNd70QP9mBWANvUfvS+AZqaKXYcN79jIJSNUjGUCDLJjcmcuB0Av7OktD5hEZb9fAfwnJTUBzf9FoKpQ=="
+    access_key           = "NczCylUeyEFgosAMgoUxbpIkT9Df9227NZshQOAgNxypAqn3UotGGzVPY6JpX0kZxfEvSDw+KE53JlZPeIyVUw=="
   }
 }
