04066492f7bb8b2480a02dd54f5414d800541773
ratheeshkumarms <ratheesh.kumarms@xe04.ey.com>
2020-07-16 19:23:46 +0530

    45

--- remote-backend.tf
+++ remote-backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "cloud-shell-storage-centralindia"
     storage_account_name = "teststroioi"
-    container_name       = "cicd"
+    container_name       = "terraform"
     key                  = "statefile.terraform.tfstate"
     access_key           = "NcSkZw+4za9VosB7N8giOR43ZLo8O3Vc64kTMeH61xusJyKtA9Fg5xSpc6Y4onqGL0zUtIIlidBwERxqgHZiAg=="
   }
