67ccc66cb5a50faad0eef5c68d31569afc1c9e3b
ratheeshkumarms <ratheesh.kumarms@xe04.ey.com>
2020-07-16 12:58:26 +0530

    uyyu

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,10 +1,10 @@
 #Remote backend to store statefiles so that all contributors work on the same configuration without conflicts
 terraform {
   backend "azurerm" {
-    resource_group_name  = "storage"
-    storage_account_name = "csg10032000cd959f89"
+    resource_group_name  = "cloud-shell-storage-centralindia"
+    storage_account_name = "cloud-shell-storage-centralindia"
     container_name       = "statefile"
     key                  = "statefile.terraform.tfstate"
-    access_key           = "NczCylUeyEFgosAMgoUxbpIkT9Df9227NZshQOAgNxypAqn3UotGGzVPY6JpX0kZxfEvSDw+KE53JlZPeIyVUw=="
+    access_key           = "yPVOcp31g4fHS+q9EmfUdB/rC3bCxopskxFNLPi5Z597/MSMD6gJocSGL9v1zAWRl2Du/kg/mRdZ5monGo+K+w=="
   }
 }
